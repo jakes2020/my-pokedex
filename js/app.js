52d4f11e72e58b8910f9687fb5a9e8f544176221
jakes2020 <19jacris@waipahuh.k12.hi.us>
2017-12-11 11:04:56 -1000

    completed

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@
 // Find the div with the id of "name1" and replace the text n/a with "Tentacool"
 // Place your code below
 
-
+document.getElementById("name1").innerHTML = "Tentacool";
 
 // Pokemon 2
 // Find the div with the id of "type2" and replace the text n/a with "water"
 // Place your code below
 
-
+document.getElementById("type2").innerHTML = "water";
 
 
 // Pokemon 3
@@ -19,7 +19,7 @@
 // Place your code below
 
 
-
+document.getElementById("pokeNumber3").innerHTML = "116";
 
 
 // Pokemon 4
@@ -27,7 +27,7 @@
 // Place your code below
 
 
-
+document.getElementById("bio4").innerHTML = "Likes to eat doodoo and sand";
 
 
 // Pokemon 5
@@ -35,7 +35,7 @@
 // Place your code below
 
 
-
+document.getElementById("bio5").style.color = "red";
 
 
 // Pokemon 6
@@ -43,11 +43,11 @@
 // Place your code below
 
 
-
+document.getElementById("name6").style.fontSize = "60px";
 
 
 // Pokemon 7
 // Find the div with the id of "container" and change the background color to "green"
 // Place your code below
 
-
+document.getElementById("container").style.backgroundColor = "green";
